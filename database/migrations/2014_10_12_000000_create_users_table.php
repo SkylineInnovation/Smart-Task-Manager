312da844f95354d81d4994136ecc4f013ba875fd
mhamad2269 <mohammadawwad22@gmail.com>
2024-08-14 12:04:22 +0300

    add the auto finish for tasks

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -27,7 +27,7 @@ return new class extends Migration
             $table->boolean('is_phone_verified')->nullable()->default(false);
 
             $table->string('fire_base_uid')->nullable();
-            $table->text('device_token')->nullable();
+            $table->string('device_token')->nullable();
 
             $table->string('fire_base_phone_uid')->nullable();
             $table->string('fire_base_google_uid')->nullable();
@@ -41,7 +41,7 @@ return new class extends Migration
 
             $table->enum('gender', ['male', 'female'])->nullable()->default('male');
             $table->string('birth_day')->nullable()->default('10/07/1997');
-            $table->text('image')->nullable()->default('assets/images/users/12.jpg');
+            $table->string('image')->nullable()->default('assets/images/users/12.jpg');
 
             $table->enum('language', ['ar', 'en'])->nullable()->default('en');
             $table->enum('status', ['active', 'blocked'])->nullable()->default('active');
