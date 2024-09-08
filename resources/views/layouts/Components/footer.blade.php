1af7167350ee68b4a1baee11de74a70c8faa4370
mhamad2269 <mohammadawwad22@gmail.com>
2024-09-08 13:04:55 +0300

    --, --,

--- resources/views/layouts/Components/footer.blade.php
+++ resources/views/layouts/Components/footer.blade.php
@@ -4,14 +4,14 @@
         <div class="row align-items-center flex-row-reverse">
             <div class="col-md-12 col-sm-12 text-center">
                 Copyright © {{ date('Y') }}
-                <a href="{{ env('COPYRIGHT_COMPANY_LINK') }}">
+                <a href="{{ env('COPYRIGHT_COMPANY_LINK') }}" target="_blank">
                     {{ env('COPYRIGHT_COMPANY_NAME') }}
-                </a>.
+                </a>
                 <br>
                 Power By
-                <a href="{{ env('POWER_COMPANY_LINK') }}">
+                <a href="{{ env('POWER_COMPANY_LINK') }}" target="_blank">
                     {{ env('POWER_COMPANY_NAME') }}
-                </a>.
+                </a>
             </div>
         </div>
     </div>
