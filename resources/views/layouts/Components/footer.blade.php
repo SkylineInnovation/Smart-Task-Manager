a98d9aba246413d5748f43b89c98cf61032a9f65
laithsobuh <sobuh97@gmail.com>
2024-10-02 12:46:19 +0300

    footer

--- resources/views/layouts/Components/footer.blade.php
+++ resources/views/layouts/Components/footer.blade.php
@@ -1,19 +1,23 @@
 <!-- FOOTER -->
+
 <footer class="footer">
     <div class="container">
-        <div class="row align-items-center flex-row-reverse">
-            <div class="col-md-12 col-sm-12 text-center">
+        <div class="row align-items-center ">
+            <div class="col-md-8 col-sm-8 text-center">
                 Copyright © {{ date('Y') }}
                 {{-- <a href="{{ env('COPYRIGHT_COMPANY_LINK', 'javascript:;') }}" class="me-3"> --}}
-                {{ env('COPYRIGHT_COMPANY_NAME', 'Codexal') }},
+                {{ env('COPYRIGHT_COMPANY_NAME', 'Codexal') }}
                 {{-- </a> --}}
                 {{-- <br> --}}
+
+
+            </div>
+            <div class="col-md-4 col-sm-4 col-4 text-end">
                 Power By
                 <a href="{{ env('POWER_COMPANY_LINK', 'javascript:;') }}" target="_blank">
                     {{-- {{ env('POWER_COMPANY_NAME', 'Codexal') }} --}}
                     <img src="{{ asset('assets/logo.png') }}" style="display: inline; height: 20px;">
                 </a>
-
             </div>
         </div>
     </div>
