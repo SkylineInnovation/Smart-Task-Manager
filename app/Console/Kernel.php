4bec151eece482b174749ee28ccbbf07bad109db
mhamad2269 <mohammadawwad22@gmail.com>
2024-10-27 16:05:16 +0300

    add command for send the urgent task reminder

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('task:auto-finish')->everyMinute();
         $schedule->command('task:auto-draft')->everyMinute();
         $schedule->command('daily:task-run')->everyMinute();
-        $schedule->command('task:urgent-reminder')->everyMinute();
+        $schedule->command('task:urgent-reminder')->dailyAt('09:00');
     }
 
     /**
