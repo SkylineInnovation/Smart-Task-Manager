63a71751528b7bbc0a155d0bf872af2920e7b5c6
M-Awwad <mohammadawwad22@gmail.com>
2025-04-29 11:23:14 +0300

    finish the tables

--- resources/views/web/repo/new-repo-page.blade.php
+++ resources/views/web/repo/new-repo-page.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.livewire-app')
 
 @section('css')
+    <link href="https://cdn.jsdelivr.net/npm/tom-select/dist/css/tom-select.bootstrap5.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/tom-select/dist/js/tom-select.complete.min.js"></script>
+
     <style>
         .mouseHover:hover {
             drop-shadow: 0 4px 8px rgba(0, 0, 0, 0.2), 0 6px 20px rgba(0, 0, 0, 0.19);
